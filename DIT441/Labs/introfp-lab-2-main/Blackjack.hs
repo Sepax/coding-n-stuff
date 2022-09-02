4dc4c4bcb53b44b46623857a00a9c570e28479a4
Sebastian <seb.plson@gmail.com>
2022-09-02 15:59:26 +0200

    feat: task A2

--- DIT441/Labs/introfp-lab-2-main/Blackjack.hs
+++ DIT441/Labs/introfp-lab-2-main/Blackjack.hs
@@ -14,7 +14,7 @@ aCard2 = Card Jack Spades
 aHand :: Hand
 aHand = [aCard1, aCard2]
 
--- Task A1 
+-- TASK A1
 
 sizeSteps :: [Int]
 sizeSteps = [ size aHand
@@ -24,5 +24,28 @@ sizeSteps = [ size aHand
             , 1 + 1 + 0
             , 2
             ]
+
+-- TASK A2
+
+-- Finds and replaces string with another string in a list https://bluebones.net/2007/01/replace-in-haskell/
+replace :: Eq a => [a] -> [a] -> [a] -> [a]
+replace [] _ _ = []
+replace s find repl =
+    if take (length find) s == find
+        then repl ++ (replace (drop (length find) s) find repl)
+        else [head s] ++ (replace (tail s) find repl)
+
+-- Displays a Card as a String
+displayCard :: Card -> String
+displayCard c = replace (show (rank c) ++ " of " ++ show (suit c)) "Numeric " ""
+
+
+-- Displays a Hand as a String
+display :: Hand -> String
+display [] = []
+display (card:cards) = displayCard card ++ "\n" ++ display cards
+
+
+
 
 
