3391e8f70693554417008e4cd05038d94e3dd4a0
Sebastian <seb.plson@gmail.com>
2022-09-02 23:16:20 +0200

    feat: task A3

--- DIT441/Labs/introfp-lab-2-main/Blackjack.hs
+++ DIT441/Labs/introfp-lab-2-main/Blackjack.hs
@@ -16,6 +16,7 @@ aHand = [aCard1, aCard2]
 
 -- TASK A1
 
+-- Returns a list which contains multiples of the values 2. Proof that the equations in said list returns the same value.
 sizeSteps :: [Int]
 sizeSteps = [ size aHand
             , size (Card (Numeric 2) Hearts : (Card Jack Spades : []))
@@ -45,7 +46,20 @@ display :: Hand -> String
 display [] = []
 display (card:cards) = displayCard card ++ "\n" ++ display cards
 
+-- TASK A3
 
+-- Converts Numeric to Int
+numToInt :: Rank -> Int
+numToInt r = case r of
+    Numeric x -> x
+
+-- Calculates the value of a given hand
+value :: Hand -> Int
+value [] = 0
+value (card: cards) = do
+    if (rank card) `elem` [Jack, Queen, King, Ace]
+        then 10 + value cards
+    else numToInt (rank card) + value cards
 
 
 
