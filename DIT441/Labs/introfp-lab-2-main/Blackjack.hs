dffb82a339627f54a669cf47a7a9e8cd2a058837
Sebastian <seb.plson@gmail.com>
2022-09-05 23:50:12 +0200

    feat: take Ace case into consideration
    Takes either the value 1 or 11 for Ace cards when calculating the value of a hand.

--- DIT441/Labs/introfp-lab-2-main/Blackjack.hs
+++ DIT441/Labs/introfp-lab-2-main/Blackjack.hs
@@ -3,7 +3,7 @@ module Blackjack where
 import Cards
 import RunGame
 
--- Test definitions
+-- Test cards
 
 cardOf2 :: Card
 cardOf2 = Card (Numeric 2) Hearts
@@ -17,6 +17,11 @@ cardOf8 = Card (Numeric 8) Hearts
 cardOfJack :: Card
 cardOfJack = Card Jack Spades
 
+cardOfAce :: Card
+cardOfAce = Card Ace Hearts
+
+-- Test hands
+
 aHand :: Hand
 aHand = [cardOf2, cardOfJack]
 
@@ -28,6 +33,9 @@ aBadHand = [cardOf4, cardOf2, cardOf2]
 
 aBustHand :: Hand
 aBustHand = [cardOfJack, cardOf8, cardOf8]
+
+anAceHand :: Hand
+anAceHand = [cardOfAce, cardOf8, cardOf8]
 
 -- TASK A1
 
@@ -64,18 +72,20 @@ display (card:cards) = displayCard card ++ "\n" ++ display cards
 
 -- TASK A3
 
--- Converts Numeric to Int
-numToInt :: Rank -> Int
-numToInt r = case r of
-    Numeric x -> x
-
 -- Calculates the value of a given hand
+
+
+-- Calculates initial value of hand when Ace has the value 11.
+preValue :: Hand -> Int
+preValue [] = 0
+preValue (card: cards) = valueRank (rank card) + preValue cards
+
+-- Calculates value of hand with Ace case taken into consideration
 value :: Hand -> Int
 value [] = 0
-value (card: cards) = do
-    if (rank card) `elem` [Jack, Queen, King, Ace]
-        then 10 + value cards
-    else numToInt (rank card) + value cards
+value (card: cards)
+    | ((valueRank (rank card)) + (preValue cards) > 21) && (rank card == Ace) = 1 + value cards --Handles Ace case 
+    | otherwise = valueRank (rank card) + value cards
 
 -- TASK A4
 
