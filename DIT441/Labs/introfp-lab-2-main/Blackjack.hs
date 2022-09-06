c0bc10b505265ceb935da911e076462638c2a73f
Sebastian <seb.plson@gmail.com>
2022-09-06 18:42:53 +0200

    feat: add test cards and hands

--- DIT441/Labs/introfp-lab-2-main/Blackjack.hs
+++ DIT441/Labs/introfp-lab-2-main/Blackjack.hs
@@ -3,9 +3,39 @@ module Blackjack where
 import Cards
 import RunGame
 
--- Test Cards
+-- Test cards
 
--- Test Hands
+cardOf2 :: Card
+cardOf2 = Card (Numeric 2) Hearts
+
+cardOf4 :: Card
+cardOf4 = Card (Numeric 4) Hearts
+
+cardOf8 :: Card
+cardOf8 = Card (Numeric 8) Hearts
+
+cardOfJack :: Card
+cardOfJack = Card Jack Spades
+
+cardOfAce :: Card
+cardOfAce = Card Ace Hearts
+
+-- Test hands
+
+aHand :: Hand
+aHand = [cardOf2, cardOfJack]
+
+aGoodHand :: Hand
+aGoodHand = [cardOf8, cardOf8, cardOf4]
+
+aBadHand :: Hand
+aBadHand = [cardOf4, cardOf2, cardOf2]
+
+aBustHand :: Hand
+aBustHand = [cardOfJack, cardOf8, cardOf8]
+
+anAceHand :: Hand
+anAceHand = [cardOfAce, cardOf8, cardOf8]
 
 -- TASK A1
 
