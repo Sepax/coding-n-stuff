59d6e0f073dafbb49eab624c351182629a0c8d60
Gustav Dalemo <gustavdalemo@hotmail.com>
2022-09-19 10:09:54 +0200

    :add exercise 0

---
+++ DIT441/Exercises/Canvas/Week 3/Exercises_week_3.hs
@@ -0,0 +1,33 @@
+-- Exercises week 3
+
+import Data.List (nub)
+import Prelude hiding (drop, splitAt, take, zip3)
+
+-- | 0. Defining Functions over Lists
+-- A.
+take :: Int -> [a] -> [a]
+take _ [] = []
+take n _ | n <= 0 = []
+take n xs | n > length xs = xs
+take n (x : xs) = x : take (n -1) xs
+
+drop :: Int -> [a] -> [a]
+drop _ [] = []
+drop n xs | n <= 0 = xs
+drop n (x : xs) = drop (n -1) xs
+
+splitAt :: Int -> [a] -> ([a], [a])
+splitAt n xs = (take n xs, drop n xs)
+
+-- B. How would you define a function zip3 which zips together three lists?
+
+-- Recursive version
+zip3 :: [a] -> [b] -> [c] -> [(a, b, c)]
+zip3 (x : xs) (y : ys) (z : zs) = (x, y, z) : zip3 xs ys zs
+zip3 _ _ _ = []
+
+-- Using zip
+zip3' :: [a] -> [b] -> [c] -> [(a, b, c)]
+zip3' xs ys zs = [(x, y, z) | ((x, y), z) <- zip (zip xs ys) zs] -- Interesting!
+
+-- Seems like zip3' ist faster when testing on large lists. Why, IDK.
