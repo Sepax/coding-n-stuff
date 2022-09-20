a872c157bc6d0c77f5a1642c091fb479daa69cd0
Gustav Dalemo <gustavdalemo@hotmail.com>
2022-09-20 11:55:34 +0200

    :add A1-A3

--- DIT441/Labs/introfp-lab-3-main/Shapes.hs
+++ DIT441/Labs/introfp-lab-3-main/Shapes.hs
@@ -1,12 +1,10 @@
-{- |
-Module      : Shapes
-Description : Types and functions for shapes. The list of all tetris pieces.
-Copyright   : (c) TDA555/DIT441, Introduction to Functional Programming
-License     : BSD
-Maintainer  : alexg@chalmers.se
-Stability   : experimental
--}
-
+-- |
+-- Module      : Shapes
+-- Description : Types and functions for shapes. The list of all tetris pieces.
+-- Copyright   : (c) TDA555/DIT441, Introduction to Functional Programming
+-- License     : BSD
+-- Maintainer  : alexg@chalmers.se
+-- Stability   : experimental
 module Shapes where
 
 import Data.List (transpose)
@@ -22,19 +20,21 @@ type Square = Maybe Colour
 
 -- | A geometric shape is represented as a list of lists of squares. Each square
 -- can be empty or filled with a block of a specific colour.
-type Row   = [Square]
-data Shape = Shape { rows :: [Row] } deriving Eq
+type Row = [Square]
+
+data Shape = Shape {rows :: [Row]} deriving (Eq)
 
 -- * Showing shapes
+
 showShape :: Shape -> String
 showShape s = unlines [showRow r | r <- rows s]
- where
-  showRow r = [showSquare s | s <- r]
-    
-  showSquare Nothing      = '.'
-  showSquare (Just Black) = '#' -- can change to '█' on linux/mac
-  showSquare (Just Grey)  = 'g' -- can change to '▓'
-  showSquare (Just c)     = head (show c)
+  where
+    showRow r = [showSquare s | s <- r]
+
+    showSquare Nothing = '.'
+    showSquare (Just Black) = '#' -- can change to '█' on linux/mac
+    showSquare (Just Grey) = 'g' -- can change to '▓'
+    showSquare (Just c) = head (show c)
 
 instance Show Shape where
   show = showShape
@@ -45,54 +45,83 @@ instance Show Shape where
 -- | All 7 tetrominoes (all combinations of 4 connected blocks),
 -- see <https://en.wikipedia.org/wiki/Tetromino>
 allShapes :: [Shape]
-allShapes = [Shape (makeSquares s) | s <- shapes] 
- where
-   makeSquares = map (map colour)
-   colour c    = lookup c [ ('I', Red),  ('J', Grey),  ('T', Blue), ('O', Yellow)
-                          , ('Z', Cyan), ('L', Green), ('S', Purple) ]
-   shapes = [["I",
-              "I",
-              "I",
-              "I"],
-             [" J",
-              " J",
-              "JJ"],
-             [" T",
-              "TT",
-              " T"],
-             ["OO",
-              "OO"],
-             [" Z",
-              "ZZ",
-              "Z "],
-             ["LL",
-              " L",
-              " L"],
-             ["S ",
-              "SS",
-              " S"]]
+allShapes = [Shape (makeSquares s) | s <- shapes]
+  where
+    makeSquares = map (map colour)
+    colour c =
+      lookup
+        c
+        [ ('I', Red),
+          ('J', Grey),
+          ('T', Blue),
+          ('O', Yellow),
+          ('Z', Cyan),
+          ('L', Green),
+          ('S', Purple)
+        ]
+    shapes =
+      [ [ "I",
+          "I",
+          "I",
+          "I"
+        ],
+        [ " J",
+          " J",
+          "JJ"
+        ],
+        [ " T",
+          "TT",
+          " T"
+        ],
+        [ "OO",
+          "OO"
+        ],
+        [ " Z",
+          "ZZ",
+          "Z "
+        ],
+        [ "LL",
+          " L",
+          " L"
+        ],
+        [ "S ",
+          "SS",
+          " S"
+        ]
+      ]
 
 -- * Some simple functions
 
 -- ** A1
+
 emptyShape :: (Int, Int) -> Shape
-emptyShape = error "A1 emptyShape undefined"
+emptyShape (m, n) = Shape (replicate n (emptyRow m))
+
+emptyRow :: Int -> Row
+emptyRow n = replicate n Nothing
 
 -- ** A2
 
 -- | The size (width and height) of a shape
 shapeSize :: Shape -> (Int, Int)
-shapeSize = error "A2 shapeSize undefined"
+shapeSize s = (length (rows s !! 0), length(rows s))
 
 -- ** A3
 
 -- | Count how many non-empty squares a shape contains
 blockCount :: Shape -> Int
-blockCount = error "A3 blockCount undefined"
+blockCount s = countAllButElem Nothing (concat(rows s))
+ where
+  countAllButElem :: Eq a => a -> [a] -> Int
+  ountAllButElem _ [] = 0
+  countAllButElem y (x:xs)
+    | y /= x = 1 + countAllButElem y xs
+    | otherwise = countAllButElem y xs
 
 -- * The Shape invariant
 
 -- ** A4
+
 -- | Shape invariant (shapes have at least one row, at least one column,
 -- and are rectangular)
 prop_Shape :: Shape -> Bool
@@ -101,6 +130,7 @@ prop_Shape = error "A4 prop_Shape undefined"
 -- * Test data generators
 
 -- ** A5
+
 -- | A random generator for colours
 genColour :: Gen Colour
 genColour = error "A5 genColour undefined"
@@ -109,6 +139,7 @@ instance Arbitrary Colour where
   arbitrary = genColour
 
 -- ** A6
+
 -- | A random generator for shapes
 genShape :: Gen Shape
 genShape = error "A6 genShape undefined"
@@ -119,21 +150,25 @@ instance Arbitrary Shape where
 -- * Transforming shapes
 
 -- ** A7
+
 -- | Rotate a shape 90 degrees
 rotateShape :: Shape -> Shape
 rotateShape = error "A7 rotateShape undefined"
 
 -- ** A8
+
 -- | shiftShape adds empty squares above and to the left of the shape
 shiftShape :: (Int, Int) -> Shape -> Shape
 shiftShape = error "A8 shiftShape undefined"
 
 -- ** A9
+
 -- | padShape adds empty sqaure below and to the right of the shape
 padShape :: (Int, Int) -> Shape -> Shape
 padShape = error "A9 padShape undefined"
 
 -- ** A10
+
 -- | pad a shape to a given size
 padShapeTo :: (Int, Int) -> Shape -> Shape
 padShapeTo = error "A10 padShapeTo undefined"
@@ -147,11 +182,13 @@ overlaps :: Shape -> Shape -> Bool
 s1 `overlaps` s2 = error "A11 overlaps undefined"
 
 -- ** B2
+
 -- | zipShapeWith, like 'zipWith' for lists
 zipShapeWith :: (Square -> Square -> Square) -> Shape -> Shape -> Shape
 zipShapeWith = error "A12 zipShapeWith undefined"
 
 -- ** B3
+
 -- | Combine two shapes. The two shapes should not overlap.
 -- The resulting shape will be big enough to fit both shapes.
 combine :: Shape -> Shape -> Shape
