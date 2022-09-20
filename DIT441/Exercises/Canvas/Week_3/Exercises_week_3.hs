3a1253555f48c3720f39673181bea97df172389e
Gustav Dalemo <gustavdalemo@hotmail.com>
2022-09-20 08:47:34 +0200

    :add exercise 3 Pascals Triangle

--- DIT441/Exercises/Canvas/Week_3/Exercises_week_3.hs
+++ DIT441/Exercises/Canvas/Week_3/Exercises_week_3.hs
@@ -1,6 +1,9 @@
 -- Exercises week 3
 
 import Data.List (nub)
+import Test.QuickCheck
+import Test.QuickCheck.Property (failed, reason, succeeded)
+--import Data.Maybe
 import Prelude hiding (drop, splitAt, take, zip3)
 
 -- | 0. Defining Functions over Lists
@@ -33,14 +36,84 @@ zip3' xs ys zs = [(x, y, z) | ((x, y), z) <- zip (zip xs ys) zs] -- Interesting!
 -- Seems like zip3' ist faster when testing on large lists. Why, IDK.
 
 -- | 1. Permutations
+-- isPermutation xs ys checks whether xs is a permutation of ys
 isPermutation :: Eq a => [a] -> [a] -> Bool
-isPermutation [] _ = True
-isPermutation (x : xs) (y : ys)
-  | x == y = isPermutation xs ys
-  | x /= y = isPermutation (x : xs) ys
+isPermutation [] [] = True
+isPermutation [] (y : ys) = False
+isPermutation (x : xs) ys = x `elem` ys && isPermutation xs (removeElemOnce x ys)
+  where
+    removeElemOnce :: Eq a => a -> [a] -> [a]
+    removeElemOnce x [] = []
+    removeElemOnce x (y : ys)
+      | x == y = ys
+      | otherwise = y : removeElemOnce x ys
+
+-- | 2. Sorting
+sorted :: Ord a => [a] -> Bool
+sorted [] = True
+sorted [x] = True
+sorted (x : y : xs) = x <= y && sorted (y : xs)
+
+insert' :: Ord a => a -> [a] -> [a]
+insert' x [] = [x]
+insert' x (y : ys)
+  | x <= y = x : y : ys
+  | otherwise = y : insert' x ys
+
+prop_insert :: Integer -> [Integer] -> Property
+prop_insert x xs = sorted xs ==> sorted (insert' x xs)
+
+-- Now use insert' to define...
+isort :: Ord a => [a] -> [a]
+isort [] = []
+isort (x : xs) = insert' x (isort xs)
 
 {-
-[1,2,1] [2,1,1]
-
-[]
+Evaluating isort [3,2,4]:
+isort [3:2:4:[]]
+insert' 3 (isort (2:4:[]))
+insert' 3 (insert' 2 (isort (4:[])))
+insert' 3 (insert' 2 (insert' 4 (isort [])))
+insert' 3 (insert' 2 (insert' 4 []))
+insert' 3 (insert' 2 (4:[]))
+insert' 3 (2:4:[])
+2: insert' 3 (4:[])
+2:3:4:[]
+[2,3,4]
 -}
+
+-- | 3*. Pascal's Triangle
+-- It Works but not the first row...
+pascal :: Int -> [Int]
+pascal 0 = []
+pascal n = [1] ++ sum2 (pascal (n -1)) ++ [1]
+  where
+    sum2 [] = []
+    sum2 [x] = [x]
+    sum2 (x : xs) = [sum [x, y] | (x, y) <- zip (x : xs) xs]
+
+{-
+Evaluating pascal 3:
+pascal 3
+[1] ++ sum2 (pascal 2) ++ [1]
+[1] ++ sum2 ([1] ++ sum2 (pascal 1) ++ [1]) ++ [1]
+[1] ++ sum2 ([1] ++ sum2 ([1] ++ sum2 (pascal 0) ++ [1]) ++ [1]) ++ [1]
+[1] ++ sum2 ([1] ++ sum2 ([1] ++ sum2 [] ++ [1]) ++ [1]) ++ [1]
+[1] ++ sum2 ([1] ++ sum2 ([1] ++ [] ++ [1]) ++ [1]) ++ [1]
+[1] ++ sum2 ([1] ++ sum2 [1,1] ++ [1]) ++ [1]
+[1] ++ sum2 ([1] ++ ([sum [x,y] | (x,y) <- zip [1,1] [1]]) ++ [1]) ++ [1]
+[1] ++ sum2 ([1] ++ [2] ++ [1]) ++ [1]
+[1] ++ sum2 [1,2,1] ++ [1]
+[1] ++ ([sum [x,y] | (x,y) <- zip [1,2,1] [2,1]]) ++ [1]
+[1] ++ [3,3] ++ [1]
+[1,3,3,1]
+-}
+
+-- Just for fun!
+pascalShow :: [[Int]] -> IO ()
+pascalShow [] = return ()
+pascalShow (x : xs) = do
+  putStrLn (show x)
+  pascalShow xs
+
+pascalList n = [pascal x | x <- [1 .. n]]
